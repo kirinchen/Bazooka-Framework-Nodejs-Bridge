a2cdac462ec78a2cb27a9b68b1afeda421896d5e
kirin <kirin.chen1001@gmail.com>
2020-01-24 20:19:27 +0800

    add BaseDataProvider

--- run/Reactiver.ts
+++ run/Reactiver.ts
@@ -3,20 +3,44 @@ import { RootReactive } from "../reactive/RootReactive";
 import { Config } from "../Config";
 
 export interface DataProvider {
-    getConfig(): Config;
+
+    get(src: string, path: string): object;
+
 }
 
-export class Reactiver implements DataProvider {
+enum BaseSrc {
+    config
 
-    config: Config;
+
+
+}
+
+export class BaseDataProvider implements DataProvider {
+
+    _config: Config;
 
     constructor(c: Config) {
-        this.config = c;
+        this._config = c;
     }
 
-    getConfig(): Config {
-        return this.config;
+    get(src: string, path: string, _default: any = null): object {
+        if (BaseSrc.config.toString() == src) return this._config.get(path, _default);
+        return null;
     }
+
+    get config(): Config { return this._config; } 
+
+
+}
+
+export class Reactiver  {
+
+    baseData: BaseDataProvider;
+
+    constructor(bdp: BaseDataProvider) {
+        this.baseData = bdp;
+    }
+
 
     observe(ws: Warder[]): RootReactive {
         let rr = new RootReactive(this, ws);
