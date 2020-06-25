a14ec5adf57bb30810ef2293733fac2a5dc832d4
Kirin <kirin-chen@domiearth.com>
2020-06-25 08:17:59 +0000

    create Bridge.ts

--- Bridge.ts
+++ Bridge.ts
@@ -1,10 +1,32 @@
 const Client = require('node-rest-client').Client;
 
+export enum VarQueryPoint {
+    flow, box
+}
+
+export interface VarQuery {
+    runFlowUid: string;
+    runBoxUid: string;
+	  point;
+
+}
 
 export class Bridge {
 
     client = new Client();
+    host: string = "http://127.0.0.1:8080/";
+    flowRunUid: string;
 
+
+
+    public init(h: string, frid: string) {
+        this.host = h;
+        this.flowRunUid = frid;
+    }
+
+    public async getVar(key: string) {
+
+    }
 
     public async getData(): Promise<string> {
 
